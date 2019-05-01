9c1f769466978dfe7bebedce5b5084b0a2f0021a
Brian Voong <bhlvoong@gmail.com>
2019-05-01 14:22:34 -0700

    Open UIColor

--- Source/Extensions+UIView.swift
+++ Source/Extensions+UIView.swift
@@ -8,12 +8,6 @@
 
 import UIKit
 
-extension UIColor {
-    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
-        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
-    }
-}
-
 // Reference Video: https://youtu.be/iqpAP7s3b-8
 extension UIView {
     
---
+++ Source/UIColor+Helpers.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Helpers.swift
+//  LBTATools
+//
+//  Created by Brian Voong on 5/1/19.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
