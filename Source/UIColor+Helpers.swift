bb36defd09d18e3538589ffb6f702cb20164e7b3
Brian Voong <bhlvoong@gmail.com>
2019-05-01 14:22:50 -0700

    Open UIColor

--- Source/UIColor+Helpers.swift
+++ Source/UIColor+Helpers.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIColor {
-    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+    static public func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
 }
