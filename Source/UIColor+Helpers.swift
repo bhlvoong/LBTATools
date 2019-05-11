981d7c47cf6e82c2cfdf25752ded19f197be109f
Ricardo Hochman <ricardohochman@hotmail.com>
2019-05-11 13:23:33 -0300

    Improving UIColor initializer with HEX

--- Source/UIColor+Helpers.swift
+++ Source/UIColor+Helpers.swift
@@ -8,7 +8,46 @@
 import UIKit
 
 extension UIColor {
-    static public func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
-        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    /**
+     Creates an UIColor from HEX String in "#363636" format
+     
+     - parameter hexString: HEX String in "#363636" format
+     - returns: UIColor from HexString
+     */
+    convenience public init(hexString: String) {
+        
+        let hexString: String = (hexString as NSString).trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString as String)
+        
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: 1)
     }
+    
+    /**
+     Creates an UIColor Object based on provided RGB value in integer
+     - parameter red:   Red Value in integer (0-255)
+     - parameter green: Green Value in integer (0-255)
+     - parameter blue:  Blue Value in integer (0-255)
+     - returns: UIColor with specified RGB values
+     */
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
 }
