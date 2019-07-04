5824143c5981c41bca41413dafa0915bbea9721d
Igor M <igor.magurean@fl3xx.com>
2019-07-04 18:17:16 +0300

    Remove text as required parameter in label init method

--- Source/Extensions/UILabel.swift
+++ Source/Extensions/UILabel.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension UILabel {
-    convenience public init(text: String?, font: UIFont? = UIFont.systemFont(ofSize: 14), textColor: UIColor = .black, textAlignment: NSTextAlignment = .left, numberOfLines: Int = 1) {
+    convenience public init(text: String? = nil, font: UIFont? = UIFont.systemFont(ofSize: 14), textColor: UIColor = .black, textAlignment: NSTextAlignment = .left, numberOfLines: Int = 1) {
         self.init()
         self.text = text
         self.font = font
