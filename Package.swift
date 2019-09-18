6375aad2f6a507efea2229acacc59dfb5689ca49
Juan Francisco <jfmiranda@gmail.com>
2019-09-18 22:23:46 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,10 +7,16 @@ let package = Package(
         .iOS(.v11)
     ],
     products: [
-        .library(name: "LBTATools", targets: ["LBTATools"]),
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "LBTATools",
+            targets: ["LBTATools"]),
     ],
     targets: [
-        .target(name: "LBTATools",
-        .testTarget(name: "LBTAToolsTests", dependencies: ["LBTATools"]
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LBTATools",
+            dependencies: []),
     ]
 )
