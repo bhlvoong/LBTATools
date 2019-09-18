3e156a5ae1c8ec404689826b3f4afc5152072d56
Juan Francisco <jfmiranda@gmail.com>
2019-09-18 22:29:47 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "LBTATools",
-    platforms: [
-        .iOS(.v11)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
