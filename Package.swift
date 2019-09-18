710c8a661cb95b163736ac217cfd1100eee5bdd5
Juan Francisco <jfmiranda@gmail.com>
2019-09-18 22:16:50 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "ZpecterLBTATools",
+    dependencies: [
+        
+    ],
     platforms: [
         .iOS(.v11)
     ],
     products: [
-        .library(name: "ZpecterLBTATools", targets: ["ZpecterLBTATools"]),
+        .library(name: "ZpecterLBTATools", targets: ["ZpecterLBTATools"], dependencies: []),
     ],
     targets: [
         .target(name: "ZpecterLBTATools",
